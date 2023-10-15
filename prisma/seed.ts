4c72d38324e26ef77a8e77679a55efbbd012f2d0
Luiz Eduardo Figueiredo <lerfbr@gmail.com>
2023-10-15 15:40:09 -0300

    Add Auth
    - Add Cors Handler
    - Add JWT Token
    - Update Prisma Model
    - Prisma Users Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,9 +14,26 @@ async function users() {
       password: await bcrypt.hash("pass4now", 10),
       firstName: "Luiz",
       lastName: "Paglioni",
+      // avatar: "https://source.unsplash.com/2FKTyJqfWX8/1000x1000",
       gender: "M",
       zipcode: "31330230",
       ntrp: 4.5,
+    },
+  });
+  
+  const pedro = await prisma.user.upsert({
+    where: { email: "pedro@courtspin.com" },
+    update: {},
+    create: {
+      userTypeId: 'PLAYER',
+      email: "pedro@courtspin.com",
+      password: await bcrypt.hash("pass4now", 10),
+      firstName: "Pedro",
+      lastName: "Rodrigues",
+      // avatar: "https://source.unsplash.com/2FKTyJqfWX8/1000x1000",
+      gender: "M",
+      zipcode: "31330230",
+      ntrp: 3.5,
     },
   });
 }
--- routes.js
+++ routes.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const routes = express.Router()
 const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('./constants')
 
 const prisma = new PrismaClient()
 
 routes.get('/', (req, res) => {
   res.json({ 'Hello': 'World' })
+})
+
+routes.post('/auth/login', async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (bcrypt.compare(password, user.password)) {
+    const { password, ...usr } = user
+
+    const token = jwt.sign({
+      id: usr.id,
+      email: usr.email
+    }, JWT_SECRET)
+
+    res.json({
+      token,
+      ...usr
+    })
+    return
+  }
+  res.status(401).json({
+    error: 'Invalid username or password'
+  })
 })
 
 routes.get('/users', async (req, res) => {
